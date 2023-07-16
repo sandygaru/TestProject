bd7a0ecdb345d64411445025a0b11ecba9948383
saisa <ssanthoshsandy92@gmail.com>
2023-07-16 16:39:04 +0530

    added val and var

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,5 +1,14 @@
 object HelloWorld {
   def main(args:Array[String]):Unit={
-    print("Hello World")
+
+    val a:Int=10
+
+    var b:String="Hi"
+
+    //a=a+10 //--> reassignment to val
+
+    print("Hello World " + a + " "+b)
+
+
   }
 }
